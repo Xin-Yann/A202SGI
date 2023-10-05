3d1e9cd34df66987529282bd4d9fe69a847ebfa5
Xin-Yann <p21013467@student.newinti.edu.my>
2023-10-05 15:58:21 +0800

    UI Adjustment

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -16,18 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select_seat_a);
+        setContentView(R.layout.home);
 
 
-
-       /*tabLayout = findViewById(R.id.tablayout);
+       tabLayout = findViewById(R.id.tablayout);
        viewPager = findViewById(R.id.viewpager);
 
        tabLayout.setupWithViewPager(viewPager);
        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
        vpAdapter.addFragment(new fragment1(), "One-Way");
        vpAdapter.addFragment(new fragment2(), "Round Trip");
-       viewPager.setAdapter(vpAdapter);*/
+       viewPager.setAdapter(vpAdapter);
     }
 
 }
