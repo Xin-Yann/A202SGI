d072ca5cb44a83beb8f9f31ed973c1baf823c8b4
HMYeoh <hongmingyeoh2002@gmail.com>
2023-10-10 16:07:01 +0800

    Profile Details A alignment

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_details_b);
+        setContentView(R.layout.profile_details_a);
 
 
        /*tabLayout = findViewById(R.id.tablayout);
